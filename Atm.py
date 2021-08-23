1476e417739734f6a4c311938182a4591e3d3497
Sahasra <73017579+sahasra09@users.noreply.github.com>
2021-08-23 17:31:23 +0530

    Update Atm.py

--- Atm.py
+++ Atm.py
@@ -1,35 +1,55 @@
-class Atm:
-    def __init__(self,cardnumber,pin):
-        self.cardnumber = cardnumber
-        self.pin = pin
+import time
+import random as ran
 
-    def check_balance(self):
-        print("Your balance is 50000")
+Name=input('Hello, what is your Name ??? - ')
 
-    def withdrawl(self,amount):
-        new_amount = 50000 - amount
-        print("you have withdrawn amount "+str(amount) +". Your remaining balance is "+ str(new_amount))
+print('Hello !!! . Welcome to this Atm .')
 
+class ATM(object):
+    def cardNumber():
+        num=input('Enter your card number - ')
+        print('Hello ' + Name + "!!!")
 
-def main():
-    Card_number = input("insert your card number:- ")
-    pin_number  = input("enter your pin number:- ")
+    def pin():
+        pin=input('Enter your pin number - ')
+        
+    def withdraw():
+        balance=100000
+        howMuchMoney=input('How much money do you want to withdraw ??? - ')
+        howMuchMoneyint=int(howMuchMoney)
+        
+        if howMuchMoneyint < balance:
+            balance=int(balance)-howMuchMoneyint
+            print('Your balance is now, '+str(balance))
+        
+        if howMuchMoneyint>balance:
+            print('Not enough money')
 
-    new_user =  Atm(Card_number ,pin_number)
+    def deposit():
+        balance=100000
+        
+        howMuchMoney=input('How much money do you want to deposit ??? - ')
+        howMuchMoneyint=int(howMuchMoney)
+       
+        balance = balance+int(howMuchMoney)
+        
+        print('Your balance in this Atm is - '+str(balance))
+    
+    def enquiry():
+        balance=str(100000)
+        print('Your balance in this Atm is - '+balance)
 
-    print("Choose your activity ")
-    print("1.Balance Enquriy   2.withdrawl")
-    activity = int(input("enter activity number :- "))
-
-    if (activity == 1):
-        new_user.check_balance()
-    elif (activity == 2):
-        amount = int(input("enter the amount:- "))
-        new_user.withdrawl(amount)
-    else:
-        print("enter a valid number")
-
-
-if __name__ == "__main__":
-    main()
-
+    cardNumber()
+    pin()
+    
+    print("What do you want to do ???")
+    transaction=str(input('if you want to withdraw some money press 1, If you want to deposit some money press 2, If you want to know how much money you have press 3 -> '))
+    
+    if transaction=='1':
+        withdraw()
+    elif transaction=='2':
+        deposit()
+    elif transaction=='3':
+        enquiry()
+    elif transaction != ('1' or '2' or '3'):
+        print('There is no transaction for the number '+ transaction)
